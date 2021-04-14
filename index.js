438af9fc8749a61350cb08c54505c3bceb07ebfa
Viktor Kainz <viktor.kainz@gmail.com>
2021-04-14 16:12:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
