ddb2ef33a8cdd79472716eebaa038b3c80222504
Koloman <moskob17@htl-kaindorf.at>
2021-04-28 16:37:18 +0200

    Added display of Hexagons with Coords

--- public/js/field.js
+++ public/js/field.js
@@ -6,7 +6,6 @@ let ctx = canvas.getContext('2d');
 
 let grass = new Image();
 grass.src = "assets/Grass.png";
-
 let mountain = new Image();
 mountain.src = "assets/Mountain.png";
 let water = new Image();
@@ -14,6 +13,93 @@ water.src = "assets/Water.png";
 let desert = new Image();
 desert.src = "assets/Desert.png";
 
+let size = 17;//nicht sicher
+
+//center von Canvas --> Koordinaten 0,0
+let hexOrigin = {x: canvas.width /2, y:canvas.height/2};
+
+//Höhe, Breite und vertikal Distanz eines Hexagons
+let hexParameter = getHexParameter();
+
+drawHexes();
+
+//Zeichnet Hexagons nebeneinander bzw. untereinander: r-> row, q->queue
+function drawHexes(){
+    let qLeftSide =  Math.round(hexOrigin.x/hexParameter.hexWidth)*4;
+    let qRightSide = Math.round((canvas.width - hexOrigin.x) / hexParameter.hexWidth)*2;
+    let rTopSide =  Math.round(hexOrigin.y/(hexParameter.hexHeight/2));
+    let rBottomSide = Math.round((canvas.height - hexOrigin.y) / (hexParameter.hexHeight/2));
+    for (let r = -rTopSide; r <=  rBottomSide; r++){
+        for (let q = -qLeftSide; q <= qRightSide; q++){
+            let center = hex_to_pixel(hex(q, r));
+            if ((center.x > hexParameter.hexWidth/2 && center.x < canvas.width - hexParameter.hexWidth/2)
+                 && (center.y > hexParameter.hexHeight/2 && center.y < canvas.height - hexParameter.hexHeight/2)){
+                drawHex(center);
+                drawHexCoordinates(center, hex(q, r));
+            }
+        }
+    }
+}
+
+//Zeichnet ein einzelnes Hexagon
+function drawHex(center){
+    //ctx.fillRect(center.x,center.y,1,1);
+    for (let i = 0; i <= 5; i++){
+        let start = hex_corner(center, i);
+        let end = hex_corner(center, i + 1);
+        drawLine({x: start.x, y: start.y}, {x: end.x, y: end.y});
+    }
+}
+
+//PunktZuPunkt-Verbindung
+function drawLine(start, end) {
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x,  end.y);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+//Schreibt Koordinaten eines Hexagons
+function drawHexCoordinates(center, hex){
+    ctx.fillText(hex.q,center.x-11, center.y);
+    ctx.fillText(hex.r,center.x+5, center.y);
+}
+
+//berechnet Eckpunkte eines Hexagons
+function hex_corner(center, i) {
+    var angle_deg = 60 * i - 30;
+    var angle_rad = Math.PI / 180 * angle_deg;
+    return point(center.x + size * Math.cos(angle_rad),
+                 center.y + size * Math.sin(angle_rad));
+}
+
+//Höhe, Breite und vertikal Distanz eines Hexagons
+function getHexParameter(){
+   let hexHeight = size * 2;
+   let hexWidth = Math.sqrt(3) * size;
+   let vertDist = hexHeight * 3/4;
+   return {hexHeight, hexWidth, vertDist};
+}
+
+//gibt den Mittelpunkt eines Hexagons zurück
+function hex_to_pixel(hex){
+    var x = size * (Math.sqrt(3) * hex.q  +  Math.sqrt(3)/2 * hex.r) + hexOrigin.x;
+    var y = size * (3./2 * hex.r) + hexOrigin.y;
+    return point(x, y);
+}
+
+//Koordinaten eines Punktes
+function point(x, y) {
+    return {x,y};
+}
+
+//Koordinaten eines Hexagons
+function hex(q,r){
+    return{q, r};
+}
+
+/* Testspielfeld
 import {Tile, types as TileTypes} from "./game/tile.js";
 import {Board} from "./game/board.js";
 
@@ -49,3 +135,4 @@ for (let i = 0; i < testarry.length; i++){
 
     }
 }
+*/
